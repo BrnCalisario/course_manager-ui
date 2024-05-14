60606f53d47065e40f3b23d15ef3d262051cd12d
BrnCalisario <bruno145224@gmail.com>
2024-05-14 14:17:21 -0300

    change font color on background brightness

--- src/app/features/schedule-page/components/schedule-week/schedule-week.component.ts
+++ src/app/features/schedule-page/components/schedule-week/schedule-week.component.ts
@@ -63,4 +63,31 @@ export class ScheduleWeekComponent implements OnInit {
 		}
 	}
 
+	public formatTextColor(backgroundColor: string) {
+
+		const color = this.hexToRgb(backgroundColor);
+
+		if (!color) return "#FFF";
+
+		const brightness = this.getBrightness(color);
+
+		return brightness > 0.7 ? "#000" : "#FFF";
+	}
+
+	private hexToRgb(hex: string) {
+		var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+		return result ? {
+			r: parseInt(result[1], 16),
+			g: parseInt(result[2], 16),
+			b: parseInt(result[3], 16)
+		} : null;
+	}
+
+	private getBrightness(rgb: { r: number, g: number, b: number }): number {
+
+		const { r, g, b } = rgb;
+
+		return (((r * 299) + (g * 587) + (b * 114)) / 1000) / 255;
+	}
+
 }
